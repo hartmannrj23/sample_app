b14054ca9c08ba66a4fc5da481702f7b1a359802
Rebecca Hartmann <hartmannrj23@uww.edu>
2012-03-16 17:32:30 -0500

    added a User factory and tests for the show page

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,14 +3,31 @@ require 'spec_helper'
 describe UsersController do
   render_views
 
+  describe "GET 'show'" do
+
+    before (:each) do
+      @user = Factory(:user)
+   end
+
+    it "should be successful", :chapter7 => true do
+      get :show, :id => @user
+      response.should be_success
+    end
+
+    it "should find the right user", :chapter7 => true do
+      get :show, :id => @user
+      assigns(:user).should == @user
+    end
+  end
+
   describe "GET 'new'" do
     it "should be successful", :chapter5 => "true" do
-      get 'new'
+      get :new
       response.should be_success
     end
 
     it "should have the right title", :chapter5 => "true" do
-      get 'new'
+      get :new
       response.should have_selector('title', :content => "Sign up")
     end
   end
---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                  "Rebecca Hartmann"
+  user.email                 "rhartmann@example.com"
+  user.password              "foobar"
+  user.password_confirmation "foobar"
+end
