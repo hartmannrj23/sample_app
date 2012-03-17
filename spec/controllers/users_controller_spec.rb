4caaba5f0d7ae7164c88403f220217a0ff78b988
Rebecca Hartmann <hartmannrj23@uww.edu>
2012-03-16 21:51:36 -0500

    Imporved the user show page and added a Gravatar

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -18,15 +18,30 @@ describe UsersController do
       get :show, :id => @user
       assigns(:user).should == @user
     end
+
+    it "should have the right title", :chapter7 => true do
+      get :show, :id => @user
+      response.should have_selector('title', :content => @user.name)
+    end
+
+    it "should have the user's name", :chapter7 => true do
+      get :show, :id => @user
+      response.should have_selector('h1', :content => @user.name)
+    end
+
+    it "should hve a profile image", :chapter7 => true do
+      get :show, :id => @user
+      response.should have_selector('h1>img', :class => "gravatar" )
+    end
   end
 
   describe "GET 'new'" do
-    it "should be successful", :chapter5 => "true" do
+    it "should be successful", :chapter5 => true do
       get :new
       response.should be_success
     end
 
-    it "should have the right title", :chapter5 => "true" do
+    it "should have the right title", :chapter5 => true do
       get :new
       response.should have_selector('title', :content => "Sign up")
     end
